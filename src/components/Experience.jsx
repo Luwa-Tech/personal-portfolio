896c267ac0f3277e20b2c8da72253ed3a3ff6b9c
Yusuf Umar Faruq <111466842+Luwa-Tech@users.noreply.github.com>
2024-11-20 19:44:42 +0000

    minor changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,13 +4,32 @@ const Experience = () => {
             <h2 className="font-poppins md:text-[3.5rem] font-extrabold relative text-[2rem] after:absolute md:after:top-[48px] after:top-[27px] after:right-[10px] after:block after:h-[.1rem] after:bg-[#d0d0f3] md:after:right-[-40px] after:w-[30%] md:after:w-[65%] ">Experience<span className="text-secondary-400">.</span></h2>
 
             <section className="mt-4">
+            <article className="border-b-[1.5px] border-[#d0d0f3] py-8">
+                    <div className="text-[1.2rem] flex items-center justify-between">
+                        <h3 className="font-bold text-[1.3rem]">Y combinator, Work at a startup</h3>
+                        <span>Oct 2024 - Nov 2024</span>
+                    </div>
+                    <div className="text-[1.2rem] flex items-center justify-between my-2">
+                        <h4 className="font-bold text-secondary-400">Full-Stack Engineer - Virtual Internship</h4>
+                        <span>Remote</span>
+                    </div>
+                    <p className="text-[1.2rem] mb-4">Added a new feature on an existing codebase of a freight shipping system to help users manage their task log easier & Increase daily active users.</p>
+
+                    <div className="text-[1.2rem] flex flex-wrap gap-4">
+                        <span className="px-1 py-1 bg-primary-800 rounded-[.8rem]">React</span>
+                        <span className="px-1 py-1 bg-primary-800 rounded-[.8rem]">Nodejs</span>
+                        <span className="px-1 py-1 bg-primary-800 rounded-[.8rem]">Analyzing user data</span>
+                        <span className="px-1 py-1 bg-primary-800 rounded-[.8rem]">Javascript</span>
+                    </div>
+                </article>
+
                 <article className="border-b-[1.5px] border-[#d0d0f3] py-8">
                     <div className="text-[1.2rem] flex items-center justify-between">
                         <h3 className="font-bold text-[1.3rem]">Tublian</h3>
                         <span>Oct 2023 - Dec 2023</span>
                     </div>
                     <div className="text-[1.2rem] flex items-center justify-between my-2">
-                        <h4 className="font-bold text-secondary-400">Open-source Developer Intern</h4>
+                        <h4 className="font-bold text-secondary-400">Software Engineer - Internship</h4>
                         <span>Remote</span>
                     </div>
                     <p className="text-[1.2rem] mb-4">Improved open-source codebases through bug fixes and documentation.</p>
@@ -29,10 +48,10 @@ const Experience = () => {
                         <span>July 2023 - Sep 2023</span>
                     </div>
                     <div className="text-[1.2rem] flex items-center justify-between my-2">
-                        <h4 className="font-bold text-secondary-400">Frontend Developer</h4>
+                        <h4 className="font-bold text-secondary-400">Frontend Engineer - Internship</h4>
                         <span>Remote</span>
                     </div>
-                    <p className="text-[1.2rem] mb-4">Led technical initiatives and srved as the product manager to achieve MVP objectives, ensuring efficient product management and clear communication.</p>
+                    <p className="text-[1.2rem] mb-4">Led technical initiatives and served as the product manager to achieve MVP objectives, ensuring efficient product management and clear communication.</p>
 
                     <div className="text-[1.2rem] flex flex-wrap gap-4">
                         <span className="px-1 py-1 bg-primary-800 rounded-[.8rem]">React</span>
